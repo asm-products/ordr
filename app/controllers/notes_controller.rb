bf08e2223c7be059382c83a2e1afd08e88e3dd84
Erik Nilsen <enilsen16@live.com>
2014-06-27 11:47:16 -0700

    fix notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,74 +1,69 @@
 class NotesController < ApplicationController
-  before_action :set_note, only: [:show, :edit, :update, :destroy]
+  before_action :set_note, only: [:edit, :update, :destroy]
+  before_action :load_notable
+  before_action :set_job
 
-  # GET /notes
-  # GET /notes.json
   def index
-    @notes = Note.all
+    @notes = @notable.notes
   end
 
-  # GET /notes/1
-  # GET /notes/1.json
   def show
   end
 
-  # GET /notes/new
   def new
-    @note = Note.new
+    @note = @notable.notes.new
   end
 
-  # GET /notes/1/edit
   def edit
   end
 
-  # POST /notes
-  # POST /notes.json
   def create
-    @note = Note.new(note_params)
-
-    respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    @note = @notable.notes.new(note_params)
+    if @note.save
+      redirect_to [@notable.job, @notable], notice: "note added."
+    else
+      instance_variable_set("@#{@resource.singularize}".to_sym, @notable)
+      render template: "#{@resource}/show"
     end
   end
 
-  # PATCH/PUT /notes/1
-  # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
       if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-        format.json { render :show, status: :ok, location: @note }
+        format.html { redirect_to [@notable.job, @notable], notice: 'note was successfully updated.' }
+        format.json { render :show, status: :ok, location: @notable }
       else
         format.html { render :edit }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
+        format.json { render json: @notable.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /notes/1
-  # DELETE /notes/1.json
   def destroy
     @note.destroy
     respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to [@notable.job, @notable], notice: "note was successfully deleted."}
+      format.json {head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @note = Note.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def note_params
-      params.require(:note).permit(:content)
-    end
+  def load_notable
+    resource, id = request.path.split('/')[3..4]
+    @notable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def set_job
+    @job = Job.find(params[:job_id])
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:content)
+  end
+
 end
